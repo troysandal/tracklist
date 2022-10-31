f9f66a3358b698cf77eadce8ac76f384f68e1188
Troy Sandal <troysandal@users.noreply.github.com>
2022-10-31 12:08:36 -0700

    Begining of Parser

---
+++ src/archive.ts
@@ -0,0 +1,45 @@
+interface ArchiveTrack {
+    key: string
+    title: string
+    artist: string
+}
+interface PlayedTime {
+    hours: number
+    minutes: number
+    seconds: number
+}
+interface PlayedDate {
+    year: number
+    month: number
+    day: number
+}
+interface PlaylistTrack {
+    key: string
+    collectionEntry: ArchiveTrack
+    playedPublic: boolean
+    startTime?: PlayedTime
+    startDate?: PlayedDate
+    startTimeJS?: Date
+    timeOffset?: number
+    timeOffsetString?: string
+}
+interface Playlist {
+    name: string
+    tracks: Array<PlaylistTrack>
+}
+interface Archive {
+    collection: { [n: string]: ArchiveTrack }
+    playlists: Array<Playlist>
+    format: string
+}
+
+interface Parser {
+    format: string
+    extensions: Array<string>
+    
+    supports(contents:string): boolean
+    parse(
+        contents: string, 
+        startTrackIndex: number, 
+        onlyPlayedTracks: boolean) : Archive | null
+}
---
+++ src/m3u.ts
@@ -0,0 +1,51 @@
+export class M3UParser implements Parser {
+    format = "M3U"
+    extensions = ['.m3u8']
+
+    supports(contents: string): boolean {
+        return contents.startsWith('#EXTM3U')
+    }
+    
+    parse(contents: string, startTrackIndex: number, onlyPlayedTracks: boolean): Archive | null {
+        return parseM3U(contents)
+    }  
+}
+
+export function parseM3U(
+    contents: string) {
+    if (!contents.startsWith('#EXTM3U')) {
+        return null
+    }
+    const archive: Archive = {
+        collection: {},
+        playlists: [] as Array<Playlist>,
+        format: 'M3U'
+    }
+    const playlist = {
+        name: 'Untitled Playlist',
+        tracks: [] as Array<PlaylistTrack>
+    }
+    archive.playlists.push(playlist)
+
+    const lines = contents.split('\n')
+
+    lines.forEach((line) => {
+        const regex = /^#EXTINF:(\d+),(([^-]*$)|(.*)( - )(.*))/g
+        const match:RegExpExecArray|null = regex.exec(line)
+        if (match && match[1]) {
+            const collectionEntry = {
+                key: match[1],
+                title: match[6] || match[3] || '',
+                artist: match[4] || ''
+            }
+            const playlistTrack = {
+                key: match[1] || '',
+                playedPublic: true,
+                collectionEntry
+            }
+            archive.collection[match[1]] = collectionEntry
+            playlist.tracks.push(playlistTrack)
+        }
+    })
+    return archive
+}
