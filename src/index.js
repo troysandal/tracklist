13542beac2064b64faf2b0ae941c1397d946c72c
Troy Sandal <troysandal@users.noreply.github.com>
2022-11-07 12:01:05 -0800

    Cleanup comments and code formatting.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { M3UParser } from './m3u'
 import { CUEParser } from './cue'
 import {RekordBoxTXTParser} from './rekordboxtxt'
 
+const DEFAULT_FORMAT_STRING = '${INDEX}. ${TITLE} - ${ARTIST}'
 const PARSERS = [TraktorParser, RekordBoxParser, M3UParser, CUEParser, RekordBoxTXTParser]
 
 const TRACK_FIELDS = {
@@ -30,7 +31,7 @@ function format(playList, trackIndex, formatString) {
 function getFormatString() {
     let formatString = document.getElementById('formatString').value
     if (formatString.length === 0) {
-        formatString = '${INDEX}. ${TITLE} - ${ARTIST}'
+        formatString = DEFAULT_FORMAT_STRING
     }
     return formatString
 }
@@ -38,27 +39,37 @@ function getFormatString() {
 /**
  * Given an archive, converts all playlists within to a human readable form.
  * 
- * @param {string} xmlText  Archive file contents.
+ * @param {string} fileContents  Archive file contents.
+ * @param {number} startTrackIndex Offset at which to start displaing tracks.
+ * @param {boolean} onlyPlayedTracks (Traktor Only) Only return tracks that 
+ *                                   were played live.
  * @returns null if Archive file is invalid
  */
- function archiveToPlaylists(xmlText, startTrackIndex, onlyPlayedTracks) {
-    // Step 1 - Convert to Canonical JSON
+ function archiveToPlaylists(fileContents, startTrackIndex, onlyPlayedTracks) {
+    // Step 1 - Find all parsers that support this file type.
     const supportedParsers = PARSERS.map((parserClass) => {
         const parser = new parserClass()
-        if (parser.supports(xmlText)) {
+        if (parser.supports(fileContents)) {
             return parser
         }
     }).filter((v) => v)
     if (!supportedParsers.length) { return null }
-    const archive = supportedParsers[0].parse(xmlText, startTrackIndex, onlyPlayedTracks)
-    if (!archive) { return null }
+
+    // Step 2 - Convert to Canonical JSON
+    // TODO startTrack and onlyLive should be per playlist, not globa.
+    const archive = supportedParsers[0].parse(fileContents, startTrackIndex, onlyPlayedTracks)
+    if (!archive) { 
+        return null
+    }
     
     // Step 3 - Walk all Playlists in the archive and make Human Readable
     let result = []
     const FORMAT_STRING = getFormatString()
 
     archive.playlists.forEach((playList) => {
-        if (result.length) { result.push('\n')}
+        if (result.length) {
+            result.push('\n')
+        }
         result.push(playList.name)
 
         playList.tracks
@@ -68,52 +79,47 @@ function getFormatString() {
     return result.join('\n')
 }
 
-function convert() {
-    const xmlText = document.getElementById('archive').value
+function convertToReadable() {
+    const fileContents = document.getElementById('archive').value
     const startTrackIndex = Math.max(1, document.getElementById('startTrackIndex').value)
     const onlyPlayPublicTracks = document.getElementById('publicTracks').checked
-    let humanReadableText = archiveToPlaylists(xmlText, startTrackIndex, onlyPlayPublicTracks)
+    let humanReadableText = archiveToPlaylists(fileContents, startTrackIndex, onlyPlayPublicTracks)
 
     if (humanReadableText === null) {
         humanReadableText = 'Bad File or No Playlist(s) found.'
     }
 
-    setTrackList(humanReadableText)
+    document.getElementById('trackList').textContent = humanReadableText
 }
 
-function setTrackList(trackList) {
-    document.getElementById('trackList').textContent = trackList
-}
-
-function upload(e) {
+function uploadTracklist(e) {
     const file = e.target.files[0]
-    if (!file) {return}
-    const reader = new FileReader();
+    if (!file) {
+        return
+    }
+    const reader = new FileReader()
 
-    reader.readAsText(file);
+    reader.readAsText(file)
     reader.onload = function () {
         const archive = document.getElementById('archive')
         archive.value = reader.result.toString()
-        convert()
+        convertToReadable()
     }
     // If you upload the same file twice the second upload won't trigger
     // this function.  Clearing the value fixes this.
     e.target.value = ''
 }
 
-/** 
- * Doesn't work - haven't debugged.
- */
 function copyToClipboard() {
     const copyText = document.getElementById('trackList').innerText
     navigator.clipboard.writeText(copyText)
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('archiveFile').addEventListener("change", upload, false)
-    document.getElementById('formatString').addEventListener("input", convert, false)
-    document.getElementById('startTrackIndex').addEventListener("input", convert, false)
-    document.getElementById('publicTracks').addEventListener("input", convert, false)
+    document.getElementById('archiveFile').addEventListener("change", uploadTracklist, false)
+    document.getElementById('formatString').addEventListener("input", convertToReadable, false)
+    document.getElementById('startTrackIndex').addEventListener("input", convertToReadable, false)
+    document.getElementById('publicTracks').addEventListener("input", convertToReadable, false)
     document.getElementById('copyToClipboard').addEventListener("click", copyToClipboard, false)
     
     const fieldList = Object.keys(TRACK_FIELDS)
