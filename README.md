21f117f76791a8b9ad1804e7f2da9d6ce356406c
troysandal <troysandal@users.noreply.github.com>
2022-11-09 14:43:27 +0000

    deploy: 4baaf34f4a680c9c7242fbb176ce32742adac8fb

--- README.md
+++
@@ -1,5 +0,0 @@
-# Traktor NML or RekordBox XML to Track List
-
-A very simple web app to convert any Traktor NML or RekordBox XML file into a
-human readable track list.  Tool is live at 
-[Heroku](https://nml2tracklist.herokuapp.com/).
--- src/common.ts
+++
@@ -1,9 +0,0 @@
-export function lineReader(contents:string, cb: (line:string, index:number) => void): void {
-    const lines = contents.split('\n')
-    lines.forEach((line, index) => {
-        line = line.trim()
-        if (line.length) {
-            cb(line, index)
-        }
-    })
-}
