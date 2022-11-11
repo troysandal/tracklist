7ee790997e0a57ba25e58c471bad37beb19c484c
Troy Sandal <troysandal@users.noreply.github.com>
2022-11-11 10:03:57 -0600

    move filtering into it's own module

---
+++ src/formatter.ts
@@ -0,0 +1,47 @@
+import { Playlist } from "./archive"
+
+export const DEFAULT_FORMAT_STRING = '${INDEX}. ${TITLE} - ${ARTIST}'
+
+export const TRACK_FIELDS = {
+    INDEX: (playList:Playlist, trackIndex:number, formatString:string): string => {
+        return formatString.replace('${INDEX}', `${trackIndex + 1}`)
+    },
+    INDEX_PADDED: (playList:Playlist, trackIndex:number, formatString:string): string => {
+        const padding = playList.tracks.length.toString().length
+        return formatString.replace('${INDEX_PADDED}', (trackIndex + 1).toString().padStart(padding, '0'))
+    },
+    TITLE: (playList:Playlist, trackIndex:number, formatString:string): string => {
+        return formatString.replace('${TITLE}', playList.tracks?.[trackIndex]?.collectionEntry.title || 'Unknown Title')
+    },
+    ARTIST: (playList:Playlist, trackIndex:number, formatString:string): string => {
+        return formatString.replace('${ARTIST}', playList.tracks?.[trackIndex]?.collectionEntry.artist || 'Unknown Artist')
+    },
+    OFFSET: (playList:Playlist, trackIndex:number, formatString:string): string => {
+        const substitution = playList.tracks[trackIndex]?.timeOffsetString ?? ''
+        return formatString.replace('${OFFSET}', substitution)
+    }
+}
+
+export function format(playList:Playlist, trackIndex:number, formatString:string): string {
+    Object.values(TRACK_FIELDS).forEach((trackKey) => { 
+        formatString = trackKey(playList, trackIndex, formatString)
+    })
+    return formatString
+}
+
+
+/**
+ * Converts a playlist to a human readable form.
+ * 
+ * @param {Playlist} playlist  Playlist to turn into a human readable string.
+ */
+export function playlistToReadable(playlist:Playlist, FORMAT_STRING:string): string {
+    const result = []
+
+    result.push(playlist.name)
+
+    playlist.tracks
+        .map((track, index) => result.push(format(playlist, index, FORMAT_STRING)))
+
+    return result.join('\n')
+}
--- src/index.js
+++ src/index.js
@@ -3,30 +3,9 @@ import { RekordBoxParser } from './rekordbox'
 import { M3U8Parser } from './m3u8'
 import { CUEParser } from './cue'
 import {RekordBoxTXTParser} from './rekordboxtxt'
+import {DEFAULT_FORMAT_STRING, playlistToReadable, TRACK_FIELDS} from './formatter'
 
-const DEFAULT_FORMAT_STRING = '${INDEX}. ${TITLE} - ${ARTIST}'
 const PARSERS = [TraktorParser, RekordBoxParser, M3U8Parser, CUEParser, RekordBoxTXTParser]
-
-const TRACK_FIELDS = {
-    INDEX: (playList, trackIndex, formatString) => formatString.replace('${INDEX}', trackIndex + 1),
-    INDEX_PADDED: (playList, trackIndex, formatString) => {
-        const padding = playList.tracks.length.toString().length
-        return formatString.replace('${INDEX_PADDED}', (trackIndex + 1).toString().padStart(padding, '0'))
-    },
-    TITLE: (playList, trackIndex, formatString) => formatString.replace('${TITLE}', playList.tracks[trackIndex].collectionEntry.title || 'Unknown Title'),
-    ARTIST: (playList, trackIndex, formatString) => formatString.replace('${ARTIST}', playList.tracks[trackIndex].collectionEntry.artist || 'Unknown Artist'),
-    OFFSET: (playList, trackIndex, formatString) => {
-        const substitution = playList.tracks[trackIndex]?.timeOffsetString ?? ''
-        return formatString.replace('${OFFSET}', substitution)
-    }
-}
-
-function format(playList, trackIndex, formatString) {
-    Object.keys(TRACK_FIELDS).forEach((trackKey) => {
-        formatString = TRACK_FIELDS[trackKey](playList, trackIndex, formatString)
-    })
-    return formatString
-}
 
 function getFormatString() {
     let formatString = document.getElementById('formatString').value
@@ -46,7 +25,7 @@ function getFormatString() {
  *                                   were played live.
  * @return Archive if parsed, otherwise null.
  */
-function parserArchive(fileContents, startTrackIndex, onlyPlayedTracks) {
+ function parserArchive(fileContents, startTrackIndex, onlyPlayedTracks) {
     const supportedParsers = PARSERS.map((parserClass) => {
         const parser = new parserClass()
         if (parser.supports(fileContents)) {
@@ -59,22 +38,6 @@ function parserArchive(fileContents, startTrackIndex, onlyPlayedTracks) {
     }
 
     return supportedParsers[0].parse(fileContents, startTrackIndex, onlyPlayedTracks)
-}
-/**
- * Converts a playlist to a human readable form.
- * 
- * @param {Playlist} playlist  Playlist to turn into a human readable string.
- */
- function playlistToReadable(playlist) {
-    const result = []
-    const FORMAT_STRING = getFormatString()
-
-    result.push(playlist.name)
-
-    playlist.tracks
-        .map((track, index) => result.push(format(playlist, index, FORMAT_STRING)))
-
-    return result.join('\n')
 }
 
 function convertToReadable() {
@@ -135,7 +98,7 @@ function updateSelectedPlaylist() {
     const onlyPlayedTracks = document.getElementById('publicTracks').checked
     const playlist = selectedOption.playlist
     const filteredPlaylist = playlist.filter(startTrackIndex, onlyPlayedTracks)
-    document.getElementById('trackList').textContent = playlistToReadable(filteredPlaylist)
+    document.getElementById('trackList').textContent = playlistToReadable(filteredPlaylist, getFormatString())
 }
 function uploadTracklist(e) {
     const file = e.target.files[0]
