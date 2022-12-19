7451f390aae5598a69ef18c40e63006a194f80f9
Troy Sandal <troysandal@users.noreply.github.com>
2022-12-19 07:43:48 -0800

    Traktor E2E Tests - public tracks - track offset

--- cypress/e2e/traktor.cy.ts
+++ cypress/e2e/traktor.cy.ts
@@ -8,4 +8,22 @@ describe('Traktor NML Files', () => {
       cy.get('pre#trackList').should('have.text', formatted)
     })
   })
+
+  it('supports track offset', () => {
+    cy.visit('http://127.0.0.1:1234')
+    cy.get('input[type=file]').selectFile('test/files/history_2022y07m17d_20h06m38s.nml')
+    cy.get('input#startTrackIndex').first().clear().type('18')
+    cy.fixture('traktor.trackoffset.txt').then((formatted) => {
+      cy.get('pre#trackList').should('have.text', formatted)
+    })
+  })
+
+  it('shows only played track', () => {
+    cy.visit('http://127.0.0.1:1234')
+    cy.get('input[type=file]').selectFile('test/files/history_2022y07m17d_20h06m38s.nml')
+    cy.get('input#publicTracks').first().check()
+    cy.fixture('traktor.publictracks.txt').then((formatted) => {
+      cy.get('pre#trackList').should('have.text', formatted)
+    })
+  })
 })
